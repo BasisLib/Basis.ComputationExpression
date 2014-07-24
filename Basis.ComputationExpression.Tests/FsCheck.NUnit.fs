660f585974ed81def8ad7a55de297bf4a704dfcd
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-07-24 16:18:10 +0900

    add tests for simple option builder. refs #2

---
+++ Basis.ComputationExpression.Tests/FsCheck.NUnit.fs
@@ -0,0 +1,31 @@
+﻿module FsCheck.NUnit
+
+open FsCheck
+open NUnit.Framework
+
+let runner =
+  { new IRunner with
+      member x.OnStartFixture t = ()
+      member x.OnArguments(ntets: int, args: obj list, every: int -> obj list -> string) = ()
+      member x.OnShrink(args, everyShrink) = ()
+      member x.OnFinished(name, result) =
+        match result with
+        | TestResult.True _ -> ()
+        | _ -> Assert.Fail(Runner.onFinishedToString name result) }
+
+let config = { Config.Default with Runner = runner }
+
+let check testable =
+  Check.One ("", config, testable)
+
+open System
+
+type NullableGen =
+  static member Nullable () =
+    Arb.fromGen (gen {
+      let! x = Arb.generate<_ option>
+      return
+        match x with Some x -> Nullable x | None -> Nullable<_>()
+    })
+
+do Arb.register<NullableGen>() |> ignore
---
+++ Basis.ComputationExpression.Tests/OptionTest.fs
@@ -0,0 +1,41 @@
+﻿module OptionTest
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open FsCheck.NUnit
+
+[<TestFixture>]
+module SimpleTest =
+  open Basis.ComputationExpression.Option.Simple
+
+  [<Test>]
+  let ``return x >>= f =  f x`` () =
+    let f x = if x = 0 then None else Some (1000 / x)
+    check (fun x -> option.Bind(option.Return(x), f) = f x)
+    check (fun x -> option { let! y = option.Return(x) in return! f y } = f x)
+
+  [<Test>]
+  let ``m >>= return  =  m`` () =
+    check (fun m -> option.Bind(m, option.Return) = m)
+    check (fun m -> option { let! x = m in return x } = m)
+
+  [<Test>]
+  let ``(m >>= f) >>= g  =  m >>= (fun x -> f x >>= g)`` () =
+    let f x = if x = 0 then None else Some (1000 / x)
+    let g x = if x < 10 then None else Some (x / 10)
+    check (fun m -> option.Bind(option.Bind(m, f), g) = option.Bind(m, fun x -> option.Bind(f x, g)))
+    check (fun m ->
+      option {
+        let! y =
+          option {
+            let! x = m
+            return! f x
+          }
+        return! g y
+      } = option {
+        let! x = m
+        let! y = f x
+        return! g y
+      }
+    )
